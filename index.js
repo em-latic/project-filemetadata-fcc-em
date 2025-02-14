777fccb26207cd42c716663368acacd9c962c5f6
em-latic <emanuel.munoz-orlich@outlook.com>
2025-02-14 01:13:34 -0600

    FIle upload and analysis

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+const multer  = require('multer')
+//const upload = multer({ dest: 'uploads/' })
+const upload = multer();
+
 var app = express();
 
 app.use(cors());
@@ -11,7 +15,18 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if(!req.file) {
+    return res.status(404).send("No file was uploaded");
+  }
+  const fileData = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  };
 
+  res.status(200).json(fileData);
+});
 
 
 const port = process.env.PORT || 3000;
